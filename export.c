7f98229cd423588ecc0fba749a7bde705cdd17ff
Guillaume Bierny <gbierny@fu-r9-p1.s19.be>
2023-01-10 22:11:54 +0100

    first commit for export

--- export.c
+++ export.c
@@ -1,2 +1,127 @@
 #include "minishl.h"
 
+
+char *ft_strndup(char *s, size_t n)
+{
+    int i;
+    char *new_s;
+    i = 0;
+    while (s[i] && i < n)
+        i++;
+    new_s = malloc((i + 1) * sizeof(char));
+    if (!new_s)
+    {
+        printf("error");
+        exit(0);
+    }
+    ft_strlcpy(new_s, s, i);
+    new_s[i] = 0;
+    return (new_s);
+}
+
+void    export(char **tab_s, char **envp)
+{
+    int i;
+
+    i = 0;
+    if (!tab_s)
+    {
+       env_in_alphabetic_order();
+       return (0);
+    }
+    while (tab_s[i])
+    {
+        envp = change_var(tab_s[i], envp);
+        i++;
+    }
+}
+
+char *name_env_var(char *s)
+{
+    int i;
+    char *new;
+    i = 0;
+    while (s[i] && s[i] != '=')
+        i++;
+    if (s[i] != '=')
+    {
+        printf("not a valid identifier");
+        exit(0);
+    }
+    new = ft_strndup(s, i + 1);
+    return (new);
+}
+
+char **add_back_tab(char **tab, char *s)
+{
+    int i;
+    char **new_tab;
+    i = 0;
+    while(tab[i])
+        i++;
+    new_tab = malloc((i + 2) * sizeof(char *));
+    i = -1;
+    while (tab[++i])
+        new_tab[i] = ft_strdup(tab[i]);
+    new_tab[i] = ft_strdup(s);
+    new_tab[i + 1] = NULL;
+    return (new_tab);
+}
+
+char **change_var(char *s, char **envp)
+{
+    int i;
+
+    i = 0;
+    i = check_arg(s);
+    if (i < 0)
+        return (envp);
+    i = -1;
+    while (envp[++i])
+    {
+        if (!ft_strncmp(name_env_var(s), name_env_var(envp[i]), ft_strlen(s)))
+            envp = erase_env_var(i, envp);
+    }
+    envp = add_back_tab(envp, s);
+    return (envp);
+}
+
+char **erase_env_var(int i, char **envp)
+{
+    int j;
+    char **new_envp;
+
+    j = 0;
+    while (envp[j])
+        j++;
+    new_envp = malloc(j * sizeof(char*));
+    if (!new_envp)
+    {
+       printf("malloc problem");
+        exit(0);
+    }
+    j = -1;
+    while(++j < i)
+        new_envp[j] = ft_strdup(envp[j]);
+    while (envp[++j])
+        new_envp[j] = ft_strdup(envp[j]);
+    new_envp[j] = NULL;
+    return (new_envp);
+}
+
+int check_arg(char *s)
+{
+    int i;
+    
+    i = 0;
+    while (s[i] && s[i] != '=')
+        i++;
+    if (s[i] == '=' && i == 0)
+    {
+        printf("invalid identifier");
+        exit(0);
+    }
+    if (s[i] == 0)
+        return (-1);
+    return (i);
+}
---
+++ sort_env.c
@@ -0,0 +1,43 @@
+void env_in_alphabetic_order(char **envp)
+{
+    int i;
+    int j;
+
+    i = 0;
+    char *front_arg;
+    char *current_arg;
+    while(envp[i])
+    {
+        j = i;
+        while (envp[j + 1])
+        {
+            if(ft_strncmp(envp[i], envp[j], ft_strlen(envp[i])) > 0)
+            envp = switch_env_var(envp, i, j);
+            j++;
+        }
+        i++;
+    }
+}
+
+char **switch_env_var(char **envp, int i, int j)
+{
+    char *tmp;
+
+    tmp = envp[i];
+
+    envp[i] = malloc(sizeof(char) * (ft_strlen(envp[j] + 1)));
+    if (!envp[i])
+    {
+       printf("malloc probleme");
+        exit(0);
+    }
+    envp[i] = envp[j];
+    envp[j] = malloc(sizeof(char) * (ft_strlen(tmp) + 1));
+    if (!envp[j])
+    {
+       printf("malloc error");
+        exit(0);
+    }
+    envp[j] = tmp;
+    return(envp);
+}
