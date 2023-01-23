efd3fdab492b474ac92ab2e8442fd1aed912ded7
Nicolas Espana ribera <niespana@fu-r3-p3.s19.be>
2023-01-23 14:55:13 +0100

    cleaned

--- pipe.c
+++ pipe.c
@@ -1,71 +1,75 @@
 #include "../includes/minishell.h"
-/* fork the process
- * return the pid on the parent side
- * on the child side
- * call ft_redirect to handle redirection of io
- * and openig/creation of files
- * the fork terminate normaly after that if no cmd nor buitin
- * call buitin if element tokenized to COMMAND is a builtin
- * call execve if cmd exist and builtin wasn't called
- */
-pid_t	exec_cmd(t_shell *minishell)
-{
-	int		proc_pid;
-	t_cmd	*cmd;
 
-	proc_pid = fork();
-	if (proc_pid > 0)
-		return (proc_pid);
-	else if (proc_pid < 0)
-		ft_exit(minishell, FAILED_FORK);
-	cmd = &minishell->cmd;
-	if (ft_redirect(minishell) == 0)
-		ft_exit(minishell, NULL);
-	if (cmd->name == NULL)
-		ft_exit(minishell, NULL);
-	if (exec_builtin(minishell))
-		ft_exit(minishell, NULL);
-	else if (cmd->path)
-	{
-		execve(cmd->path, cmd->arg, cmd->envp);
-		ft_exit(minishell, FAILED_EXEC);
+char *get_path(char *cmd) {
+	int i;
+	char **paths;
+	char *cmd_path;
+	char *res;
+
+	cmd_path = ft_strjoin("/", cmd);
+	i = -1;
+	paths = ft_split(getenv("PATH"), ':');
+	res = ft_strjoin(paths[i], cmd_path);
+	while (access(res, X_OK) && paths[++i]) {
+		free(res);
+		res = ft_strjoin(paths[i], cmd_path);
 	}
-	ft_exit(minishell, NULL);
-	return (-1);
+	free(cmd_path);
+	i = -1;
+	while (paths[++i])
+		free(paths[i]);
+	free(paths);
+	return (res);
 }
 
-/* set up the pipes and execute the command
- * and perform the neccessary close()
- * each iteration of the loop will creat a pipe except the last
- * each iteration of the loop will set the fd_in to the read end
- * of the pipe except the first
- * each iteration of the loop will set the fd_out to the write end
- * of the pipe except the last
- */
-void	exec_pipeline(t_shell *minishell)
+void	exec_cmds(t_list *cmds, int fds[2], char **env)
 {
-	int	i;
-	int	fd[2];
+	char	**crt;
+	int		i;
+	char	*cmd;
+	pid_t	pid;
+	int k = -1;
+
+	if (pipe(fds) == -1)
+		exit(0);
+	crt = (char **)cmds->content;
+	while (crt[++k])
+		printf("%d %s\n", k, crt[k]);
+	cmd = get_path(crt[0]);
+	printf("%s\n", cmd);
+	pid = fork();
+	if (!pid)
+		execve(cmd, crt, env);
+	else
+	{
+		i = -1;
+		free(cmd);
+		if (cmds->next)
+		{
+			while (crt[++i])
+				free(crt[i]);
+			free(crt);
+			exec_cmds(cmds->next, fds, env);
+		}
+	}
+}
+
+int main(int argc, char **argv, char **env)
+{
+	t_list *cmds;
+	char **split;
+	int fd_s[2];
+	int i;
 
 	i = 0;
-	while (i <= minishell->nbr_pipe)
+	fd_s[0] = 0;
+	fd_s[1] = 1;
+
+	cmds = ft_lstnew(NULL);
+	while (argv[++i])
 	{
-		ft_build_cmd(minishell);
-		if (i > 0)
-			(minishell->cmd).fd_in = fd[0];
-		if (i < minishell->nbr_pipe)
-		{
-			if (pipe((fd)) == -1)
-				ft_exit(minishell, FAILED_PIPE);
-			(minishell->cmd).fd_out = fd[1];
-		}
-		(minishell->pid)[i] = exec_cmd(minishell);
-		if (i < minishell->nbr_pipe)
-			close(fd[1]);
-		if (i > 0)
-			close((minishell->cmd).fd_in);
-		move_pipeline(minishell);
-		reset_cmd(minishell);
-		i++;
+		split = ft_split(argv[i], ' ');
+		ft_lstadd_back(&cmds, ft_lstnew(split));
 	}
+	exec_cmds(cmds->next, fd_s, env);
 }
